1e8b86244eabfd042e9185773afbbdb0b293e8f2
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-03-02 12:03:17 -0500

    os-hostname
    os-hostname

---
+++ os-hostname/main.js
@@ -0,0 +1,6 @@
+// Importamos el módulo 'os' de Node.js, que proporciona funciones para obtener información del sistema operativo.
+const os = require('node:os');  
+
+// Llamamos a la función `hostname()` del módulo 'os', 
+// la cual devuelve el nombre del host (nombre del equipo o computadora).
+console.log(os.hostname());  
